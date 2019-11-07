cec7aead7d3a91c66c1d96bc514be349a37b5ed6
Andrii <farikpite@gmail.com>
2019-11-07 19:02:41 +0200

    Updates

---
+++ precache-manifest.bf851b3d32dd26b9150563efa7a6345a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65fce3d29cb0eb396aee6bd04ac4bc91",
+    "url": "/Andrii-Pavlenko/todoApp1/index.html"
+  },
+  {
+    "revision": "8e0085cd9f0de3fe2688",
+    "url": "/Andrii-Pavlenko/todoApp1/static/css/main.afee8014.chunk.css"
+  },
+  {
+    "revision": "c20698d34b12af4f9d70",
+    "url": "/Andrii-Pavlenko/todoApp1/static/js/2.3e5eadab.chunk.js"
+  },
+  {
+    "revision": "8e0085cd9f0de3fe2688",
+    "url": "/Andrii-Pavlenko/todoApp1/static/js/main.904eb94d.chunk.js"
+  },
+  {
+    "revision": "b9144a632f8a86343d10",
+    "url": "/Andrii-Pavlenko/todoApp1/static/js/runtime~main.c0b3792d.js"
+  }
+]);
