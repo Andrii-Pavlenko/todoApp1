b9fadbb10b0159ce5ba46db33071b72e3a64debf
Andrii <farikpite@gmail.com>
2019-11-07 20:24:40 +0200

    Updates

---
+++ precache-manifest.8c0cff457174ea353ca91ce98fe71931.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "057ab95459a027824f77f1d74c147444",
+    "url": "/todoApp1/index.html"
+  },
+  {
+    "revision": "8e0085cd9f0de3fe2688",
+    "url": "/todoApp1/static/css/main.afee8014.chunk.css"
+  },
+  {
+    "revision": "c20698d34b12af4f9d70",
+    "url": "/todoApp1/static/js/2.3e5eadab.chunk.js"
+  },
+  {
+    "revision": "8e0085cd9f0de3fe2688",
+    "url": "/todoApp1/static/js/main.904eb94d.chunk.js"
+  },
+  {
+    "revision": "1eb503f3ad34699759fe",
+    "url": "/todoApp1/static/js/runtime~main.fbeaeec6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Andrii-Pavlenko/todoApp1/precache-manifest.bf851b3d32dd26b9150563efa7a6345a.js"
+  "/todoApp1/precache-manifest.8c0cff457174ea353ca91ce98fe71931.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Andrii-Pavlenko/todoApp1/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todoApp1/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
